2b50708264de1bac7b10dba483938c5567c8b4cc
Samuel Egea <samuelegea@hotmail.com>
2022-12-15 22:49:02 -0300

    Add ed a dynamic filter_by_not filters and a define_filter function to help create more automatic filters, and documentation for this features

--- lib/active_record/filterable/active_record_extension.rb
+++ lib/active_record/filterable/active_record_extension.rb
@@ -15,9 +15,67 @@ module ActiveRecord
           end
 
           # Testing the not operator
-          # define_method("filter_by_not_#{column.name}") do |filter|
-          #   where.not(column.name.to_sym => filter)
+          # This defines the not operator, so you can filter by a column that is not equal to a value
+          # Example:
+          # User.filter_by_not_name("Jhon").to_sql
+          # => "SELECT \"users\".* FROM \"users\" WHERE \"users\".\"name\" != 'Jhon'"
+          # User.filter_by_not_deleted_at(nil).to_sql
+          # => "SELECT \"users\".* FROM \"users\" WHERE \"users\".\"deleted_at\" IS NOT NULL"
+
+          define_method("filter_by_not_#{column.name}") do |filter|
+            where.not(column.name.to_sym => filter)
+          end
+
+          # Define a define_filter helper, to define the class methods in a more readable way,
+          # You can always define a class method started with "filter_by_", or use the scope method, from Rails
+          # And even use the filter method equally to the scope method, with lambda or proc, the same rules apply
+          # Either way, you must return an ActiveRecord::Relation at the end of the method
+
+          # Example:
+          # define_filter :name_of_child, ->(name) { joins(:childs).where(child: { name: name }) }
+          # will generate a class method like User#filter_by_name
+          # that will perform the query: User.joins(:childs).where(child: { name: name })
+          # You can also define a block, that will be extended to the scope, so you can define more methods
+          # Example:
+          # define_filter :name_of_child, ->(name) { joins(:childs).where(child: { name: name }) } do
+          #   def with_age(age)
+          #     where(child: { age: age })
+          #   end
           # end
+          # will generate a class method like User#filter_by_name_of_child
+          # Using like User.filter_by_name_of_child("Jhon").with_age(10)
+          # that will return a query like: 
+          # => "SELECT \"users\".* FROM \"users\" INNER JOIN \"childs\" ON \"childs\".\"user_id\" = \"users\".\"id\" WHERE \"childs\".\"name\" = 'Jhon' AND \"childs\".\"age\" = 10"
+          # Any method defined in the block will be available in the scope
+          # Althought it would not be very advisable because the #filter won't catch these block methods,
+          # it can be useful elsewhere, so you can use it as you wish.
+
+          def define_filter(name, body, &block)
+            raise ArgumentError, 'The scope body needs to be callable.' unless body.respond_to?(:call)
+
+            if dangerous_class_method?("filter_by_#{name}")
+              raise ArgumentError, "You tried to define a filter named \"#{name}\" " \
+                "on the model \"#{self.name}\", but Active Record already defined " \
+                'a class method with the same name.'
+            end
+
+            extension = Module.new(&block) if block
+
+            if body.respond_to?(:to_proc)
+              singleton_class.define_method("filter_by_#{name}") do |*args|
+                scope = all._exec_scope(*args, &body)
+                scope = scope.extending(extension) if extension
+                scope
+              end
+            else
+              singleton_class.define_method("filter_by_#{name}") do |*args|
+                scope = body.call(*args) || all
+                scope = scope.extending(extension) if extension
+                scope
+              end
+            end
+            singleton_class.send(:ruby2_keywords, "filter_by_#{name}")
+          end
         end
       end
     end
--- spec/support/models/user.rb
+++ spec/support/models/user.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
-# class User < ActiveRecord::Base
-#   has_many :posts, class_name: 'Post', foreign_key: 'author_id'
-#   has_many :likes, class_name: 'Like', foreign_key: 'author_id'
-#   has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
-
-# end
 class User < ActiveRecord::Base
-  act_as_filterable
+  # act_as_filterable
+  include ActiveRecord::Filterable
+  has_many :posts, class_name: 'Post', foreign_key: 'author_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'author_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
+
+  define_filter :posts_that_have_more_than_n_likes, ->(number_of_likes) { joins(posts: :likes).group('posts.id').having('count(likes.id) > ?', number_of_likes) } do
+    def newer_than(date)
+      where('posts.created_at > ?', date)
+    end
+
+    def older_than(date)
+      where('posts.created_at < ?', date)
+    end
+  end
 end
